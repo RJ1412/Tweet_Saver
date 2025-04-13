7507b0b20d7b87be15d2e065eb3e2747b78cbc8b
Rahul Raj <135814536+RJ1412@users.noreply.github.com>
2025-04-14 03:03:44 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,21 +1,8 @@
+document.getElementById("saveBtn").addEventListener("click", () => {
+  const token = document.getElementById("token").value;
+  const pageId = document.getElementById("pageId").value;
 
-document.getElementById("saveTweet").addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
+  chrome.storage.local.set({ NOTION_TOKEN: token, NOTION_PAGE_ID: pageId }, () => {
+    window.close();
+  });
 });
-
-
-document.getElementById("saveTweet").addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  if (/^(https?:\/\/)?(www\.)?(twitter\.com|x\.com)\//.test(tab.url)) 
-    {
-    chrome.runtime.sendMessage({ tweetUrl: tab.url }, () => {
-      document.getElementById("status").style.display = "block";
-    });
-  } else {
-    alert("Not a Twitter page!");
-  }
-});
-
-
