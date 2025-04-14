12ad6bad73048ac3a04eaaa19b97a5ddf4f2e062
Rahul Raj <135814536+RJ1412@users.noreply.github.com>
2025-04-14 19:40:52 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.tweetUrl && message.tweetText) {
+  if (message.tweetUrl) {
     chrome.storage.local.get(["NOTION_TOKEN", "NOTION_PAGE_ID"], (data) => {
       const token = data.NOTION_TOKEN;
       const pageId = data.NOTION_PAGE_ID;
@@ -9,6 +9,18 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         return;
       }
 
+      const { tweetUrl } = message;
+
+      const children = [
+        {
+          object: "block",
+          type: "embed",
+          embed: {
+            url: tweetUrl
+          }
+        }
+      ];
+
       fetch(`https://api.notion.com/v1/blocks/${pageId}/children`, {
         method: "PATCH",
         headers: {
@@ -17,51 +29,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
           "Notion-Version": "2022-06-28"
         },
         body: JSON.stringify({
-          children: [
-            {
-              object: "block",
-              type: "bulleted_list_item",
-              bulleted_list_item: {
-                rich_text: [
-                  {
-                    type: "text",
-                    text: {
-                      content: `Tweet: ${message.tweetText}`
-                    }
-                  }
-                ]
-              }
-            },
-            {
-              object: "block",
-              type: "bulleted_list_item",
-              bulleted_list_item: {
-                rich_text: [
-                  {
-                    type: "text",
-                    text: {
-                      content: "Link",
-                      link: {
-                        url: message.tweetUrl
-                      }
-                    }
-                  }
-                ]
-              }
-            },
-            {
-              object: "block",
-              type: "paragraph",
-              paragraph: {
-                rich_text: []
-              }
-            }
-          ]
+          children
         })
       })
         .then(response => response.json())
         .then(data => {
-          console.log("Tweet snippet saved to Notion:", data);
+          console.log("Tweet embedded in Notion:", data);
           sendResponse({ success: true });
         })
         .catch(error => {
@@ -70,6 +43,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         });
     });
 
-    return true; 
+    return true;
   }
 });
